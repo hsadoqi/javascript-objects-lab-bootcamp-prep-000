14aa8d60dcbc62e9954b696b98a4207d607c83d4
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-07-15 21:05:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,3 @@ function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
-
-function updateObjectWithKeyAndValue(object, key, value){
-  var new_object = object.slice
-  new_object[key] = value
-  return new_object
-}
