0986bd139fe30e5fdbcffd174384b97ac3a239d0
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-07-15 21:08:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {prop: 1};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
-  return object
+  var new_object = Object.assign({}, {object[key] = value})
+  return new_object
 }
